e876489566b81b4ead770388cf03d176e780171c
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-08-21 21:15:42 +0800

    add: first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+import TextEditor from "./TextEditor";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to={`/documents/${uuidV4()}`} />
+        </Route>
+        <Route path="/documents/:id">
+          <TextEditor />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ server/Document.js
@@ -0,0 +1,9 @@
+// schema
+const { Schema, model } = require("mongoose");
+
+const Document = new Schema({
+  _id: String,
+  data: Object,
+});
+
+module.exports = model("Document", Document);
